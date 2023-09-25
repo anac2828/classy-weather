83c640afb999af4589da317f57c6d03600d4799b
Ana MacBook <anac2828@yahoo.com>
2023-09-24 20:27:27 -0700

    displaying fetched data on browser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getWeatherIcon(wmoCode) {
+  const icons = new Map([
+    [[0], '☀️'],
+    [[1], '🌤'],
+    [[2], '⛅️'],
+    [[3], '☁️'],
+    [[45, 48], '🌫'],
+    [[51, 56, 61, 66, 80], '🌦'],
+    [[53, 55, 63, 65, 57, 67, 81, 82], '🌧'],
+    [[71, 73, 75, 77, 85, 86], '🌨'],
+    [[95], '🌩'],
+    [[96, 99], '⛈'],
+  ]);
+  const arr = [...icons.keys()].find((key) => key.includes(wmoCode));
+  if (!arr) return 'NOT FOUND';
+  return icons.get(arr);
+}
+
+function convertToFlag(countryCode) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map((char) => 127397 + char.charCodeAt());
+  console.log(codePoints);
+  return String.fromCodePoint(...codePoints);
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: 'lisbon',
+      isLoading: false,
+      displayLocation: '',
+      weather: {},
+    };
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
+
+  async fetchWeather() {
+    try {
+      this.setState({ isLoading: true });
+      // 1) Getting location (geocoding)
+      const geoRes = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${this.state.location}`
+      );
+      const geoData = await geoRes.json();
+      console.log(geoData);
+
+      if (!geoData.results) throw new Error('Location not found');
+
+      const { latitude, longitude, timezone, name, country_code } =
+        geoData.results.at(0);
+      this.setState({
+        displayLocation: `${name} ${convertToFlag(country_code)}`,
+      });
+
+      // 2) Getting actual weather
+      const weatherRes = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+      );
+      const weatherData = await weatherRes.json();
+      this.setState({ weather: weatherData.daily });
+      console.log(this.state);
+    } catch (err) {
+      console.err(err);
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <h1>Classy Weather</h1>
+        <input
+          type='text'
+          placeholder='Search from location'
+          value={this.state.location}
+          onChange={(e) => this.setState({ location: e.target.value })}
+        />
+        <button onClick={this.fetchWeather}>Get weather</button>
+
+        {this.state.isLoading && <p className='loader'>Loading...</p>}
+
+        {this.state.weather.weathercode && (
+          <Weather
+            location={this.state.displayLocation}
+            weather={this.state.weather}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
+
+class Weather extends React.Component {
+  render() {
+    console.log(this.props.weather);
+    const {
+      temperature_2m_max: max,
+      temperature_2m_min: min,
+      time: dates,
+      weathercode: codes,
+    } = this.props.weather;
+    return (
+      <div>
+        <h2>{this.props.location}</h2>
+      </div>
+    );
+  }
+}
---
+++ src/Counter.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 5 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+  }
+
+  handleDecrement() {
+    this.setState((curState) => {
+      return { count: curState.count - 1 };
+    });
+  }
+
+  handleIncrement() {
+    this.setState((curState) => {
+      return { count: curState.count + 1 };
+    });
+  }
+
+  render() {
+    const date = new Date('June 21 2027');
+    date.setDate(date.getDate() + this.state.count);
+    console.log();
+    return (
+      <div>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>
+          {date.toDateString()} [{this.state.count}]
+        </span>
+        <button onClick={this.handleIncrement.bind(this)}>+</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
